0c7a75f4d139b753de633592653d38a3789eeb26
TanguyVaucher <1999tanguy@gmail.com>
2026-03-21 02:17:58 +0100

    slr

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import CrashTitles from './components/CrashTitles.tsx';
 import BottomSectionNav from './components/BottomSectionNav.tsx';
 import SpaSafety from './components/SpaSafety.tsx';
 import shaderFrontUrl from './assets/f1-merco.avif';
-import shaderBackUrl from './assets/shader-back.jpg';
+import shaderBackUrl from './assets/verso-srl.avif';
 import studioGlbUrl from './models/studio.glb';
 import ferrariGlbUrl from './models/Ferrari_SF26_2026.glb';
 
