3caef384825d3f901a588b4e982f1e3d647b7988
TanguyVaucher <1999tanguy@gmail.com>
2026-04-11 01:36:28 +0200

    -

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
   },
